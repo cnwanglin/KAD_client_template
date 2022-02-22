862409779ae932762c85af91e19f4f310b69536b
wzc2019 <751622806@qq.com>
2022-02-22 17:00:39 +0800

    完善记录曲线，报警子类型

--- src/main/java/cn/kad/constant/CSMConstant.java
+++ src/main/java/cn/kad/constant/CSMConstant.java
@@ -74,18 +74,60 @@ public class CSMConstant {
 
     //记录曲线分类
     public static class CSMHistoricalCurveType{
-        public static final int TYPE_TW_ACTION_CURVE =0X10;//道岔动作曲线
-        /**
-         * more
-         */
+        public static final int TYPE_ACTION_CURVE =0X10;//道岔动作曲线
+        public static final int TYPE_FUEL_PRESSURE =0X11;    //道岔油压
+        public static final int TYPE_PULL =0X12;    //道岔拉力
+        public static final int TYPE_HIGH_VOLT_WAVE =0X13;  //高压不对成波形
+        public static final int TYPE_SUDDEN_OFF_CURVE =0X20;    //外电网瞬间断电曲线
+        public static final int TYPE_FUEL_PRESSURE_CURVE =0X21; //油压曲线
+        public static final int TYPE_INPUT_MUTATION_CRUVE =0X22; //电源屏输入突变曲线
     }
 
     //报警子类型
     public static class CSMSignalEquipmentAlarmType{
-        public static final int TYPE_SJ_LOCK =0X01;//SJ锁闭封联报警
-        /**
-         * more
-         */
+        public static final int TYPE_SJ_ALARM =0X01;//SJ锁闭封联报警
+        public static final int TYPE_TW_REPRESENT_NOT_SAME =0X02;//道岔表示不一致（保留）
+        public static final int TYPE_TW_CROWD =0X03;//挤岔
+        public static final int TYPE_TRAIN_ABNORMAL_CLOSE =0X04;//列车信号非正常关闭
+        public static final int TYPE_FIRE =0X05;//火灾
+        public static final int TYPE_BREAKDOWN_BTN =0X06;//故障通知按钮
+        public static final int TYPE_INPUT_POWER_OFF_ALARM =0X07;//外电网输入电源断相/断电报警
+        public static final int TYPE_THREE_POWER_WRONG_SEQ =0X08;//外电网三相电源错序报警
+        public static final int TYPE_INPUT_POWER_SUDDEN_OFF_ALARM =0X09;//外电网输入电源瞬间断电报警
+        public static final int TYPE_TRAIN_BROKEN_WIRE_ALARM =0X0A;//列车主灯丝断丝报警
+        public static final int TYPE_BROKEN_WIRE_ALARM =0X0B;//熔丝断丝报警
+        public static final int TYPE_SWITCH_MACHINE_REPRESENT_GAP_ALARM =0X0C;//转辙机表示缺口报警
+        public static final int TYPE_INTREVAL_EQUITMENT_ALARM=0X0D;//区间设备报警
+        public static final int TYPE_SIGNAL_CENTRAL_COLLECT_ALARM =0X0E;//信号集中检测内部采集通信故障报警
+        public static final int TYPE_ENV_ALARM =0X0F;//环境监测报警
+        public static final int TYPE_TDCS_CTC_ALARM =0X10;//TDCS/CTC系统报警
+        public static final int TYPE_TRAIN_CONTRL_ALARM =0X11;//列控系统报警
+        public static final int TYPE_UNION_lOCK_ALARM =0X12;//计算机联锁系统报警
+        public static final int TYPE_ELECTRIC_LIMIT_ALARM =0X13;//电气特性超限报警
+        public static final int TYPE_OTHER_COMMUNICATION_ALARM =0X14;//检测御其他系统通信接口故障报警
+        public static final int TYPE_TW_COUNT_LIMIT =0X16;//道岔运用次数超限报警
+        public static final int TYPE_COMPENSATION_CAPACITANCE_ALARM =0X17;//补偿电容断线预警
+        public static final int TYPE_ZPW_ALARM =0X18;//ZPW-2000 系统报警应答器
+
+        public static final int TYPE_RESPONSER_ALARM =0X1A;//应答器报警
+        public static final int TYPE_TV_ALARM =0X1B;//智能电源屏报警
+        public static final int TYPE_INTREVAL_WATCH_ALARM =0X1C;//区间综合监控系统报警
+        public static final int TYPE_DISASTER_ALIEN_ALARM =0X22;//防灾异物侵限报警
+        public static final int TYPE_TSRS_ALARM =0X26;//TSRS设备报警
+        public static final int TYPE_RBC_ALARM =0X27;//RBC报警
+        public static final int TYPE_TV_OUT_OFF_ALARM =0X28;//电源屏输出断电报警
+        public static final int TYPE_TW_NO_PRESENT_ALARM =0X29;//道岔无表示报警
+        public static final int TYPE_ANALOG_PREDICT =0X30;//各种设备模拟量变化趋势、突变、异常波动预警
+        public static final int TYPE_CONTROL_BTN_RECORD =0X53;//控制台按钮操作记录
+        public static final int TYPE_TW_LONG_BUSY_ALARM =0X58;//轨道长时间占用报警
+        public static final int TYPE_ANALYSIS_PRIDICT =0X62;//智能分析预警（预留）
+        public static final int TYPE_ANALYSIS_DIAGNOSIS =0X64;//智能分析故障诊断（预留）
+        public static final int TYPE_TW_STATE_NOT_SAME_ALARM =0X70;//ZPW-2000 轨道状态不一致报警
+        public static final int TYPE_TWO_POWER_OFF =0X90;//外电网双路断电报警
+        public static final int TYPE_OUT_WATCH_PREDICT =0XA0;//ZPW-2000 区间轨道电路室外监控预警
+        public static final int TYPE_OUT_WATCH_ALARM =0XA1;//ZPW-2000 区间轨道电路室外监控报警
+        public static final int TYPE_TV_OUT_OFF_PHASE =0XA2;//电源屏输出断相
+        public static final int TYPE_TV_OUT_WRONG_SEQ =0XA3;//电源屏输出错序
     }
 
     //工作日志子类型
